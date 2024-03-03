b3f683adb697d60660f4bd498df6208566b69d83
Sepul <ssepulveda119@gmail.com>
2024-03-04 00:26:31 +0100

    ,

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
-import express from 'express';
-import{ router } from './rutas/router.js'
+import express from "express";
+import { router } from "./rutas/router.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
-
-app.use('/',router)
-
+app.use("/", router);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
